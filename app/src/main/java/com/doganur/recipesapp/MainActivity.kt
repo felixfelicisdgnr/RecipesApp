fe23752816b7d0a49f442502e54f615d2785d3db
Doganur <doganuraydeniz@hotmail.com>
2023-05-02 02:10:42 +0300

    first change with retrofit

--- app/src/main/java/com/doganur/recipesapp/MainActivity.kt
+++ app/src/main/java/com/doganur/recipesapp/MainActivity.kt
@@ -5,39 +5,24 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.doganur.recipesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val navController by lazy { findNavController(R.id.hostFragment) }
+        binding.btnNav.setupWithNavController(navController)
+
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-
-        val menuInflater = menuInflater
-        menuInflater.inflate(R.menu.add_meal,menu)
-
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        /*
-        herhangi bir ıtem seçilirse ne yapacağız onu burada yapıyoruz
-        birden fazla item olabilir bu yüzden item kontrol etme alışkanlığı kazanmalıyım, if ile kontrol etmeliyim
-        tıklanan yerin id sini kontrol ediyoruz,genelde optionsItemlarda birden fazla seçenek olabiliyor
-
-        ->yemek tariflerinin olduğu fragmenta gideceğiz yani definition fragmenta gideceğiz,
-
-         */
-        if (item.itemId == R.id.add_meal_item){
-            val action = ListFragmentDirections.actionListFragmentToDefinitionFragment("icomemenu",0)
-            // ListFragmentDirections bize hazır olarak verilen fonksiyon, devamı da aksiyonu seçtiğimizde hazır oluşturulan obje
-            Navigation.findNavController(this,R.id.fragmentContainerView).navigate(action)
-        //Navigationı çağırdım, findNavControllerın içinde activityde olduğumu belirttim sonra görünümün id sıni istedi
-        //mainactivity içerisindeki navHost fragmentın id sini verdim, .navigate diyerek (istediğimiz yere gidebilirdik ama biz actiona gitmek istedik)
-        // nereye gideceğimzii belirttim
-        }
-
-        return super.onOptionsItemSelected(item)
-    }
 }
