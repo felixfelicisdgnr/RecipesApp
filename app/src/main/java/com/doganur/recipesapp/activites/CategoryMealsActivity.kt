7f9e1319444932366223b1c5df511847b04ee7e6
Doganur <doganuraydeniz@hotmail.com>
2023-05-08 23:37:24 +0300

    fifth change update

---
+++ app/src/main/java/com/doganur/recipesapp/activites/CategoryMealsActivity.kt
@@ -0,0 +1,12 @@
+package com.doganur.recipesapp.activites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.doganur.recipesapp.R
+
+class CategoryMealsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category_meals)
+    }
+}
