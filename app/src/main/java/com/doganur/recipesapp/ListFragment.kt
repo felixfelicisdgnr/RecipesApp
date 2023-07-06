b2bf6563e8e32167104e9ff5011f5ab50254c479
Doganur <doganuraydeniz@hotmail.com>
2023-07-06 14:23:03 +0300

    Last Commit

--- app/src/main/java/com/doganur/recipesapp/ListFragment.kt
+++ app/src/main/java/com/doganur/recipesapp/ListFragment.kt
@@ -17,10 +17,6 @@ class ListFragment : Fragment() {
 
     private lateinit var listAdapter: RecyclerListAdapter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
